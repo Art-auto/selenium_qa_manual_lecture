cdf1500c661dfa5518d73e256cc43406ebdac707
Oleksandr Golubishko <oleksandr.golubishko@ab-soft.net>
2023-03-31 12:38:52 +0300

    Add ChromeOptions to six problem with driver

--- src/test/java/ua/ithillel/automation/ui/SimpleUiTest.java
+++ src/test/java/ua/ithillel/automation/ui/SimpleUiTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -38,7 +39,9 @@ public class SimpleUiTest {
      */
     @Before
     public void precondition() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
